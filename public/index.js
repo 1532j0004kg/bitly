5392ef470627a23382e0217257508f206c19c99f
Glitch (maxiurl) <none>
2019-03-13 11:54:52 +0000

    Database code removed and UI changed to black BG

--- public/index.js
+++ public/index.js
@@ -5,26 +5,15 @@ $("#shorten").click(function(){
     url: "https://api-ssl.bitly.com/v3/shorten?longUrl="+longurl+"&access_token=57f0a8845124ac6c2b207ea57f84daa3b68ef2fc",
     type: "POST",
     success: function(res){
-      $.ajax({
-            url: "/post",
-            type: "POST",
-            data: {
-              url : res.data.url
-            },
-            success: function(res){
-              console.log(res);
                 if(res.url)
                   $("#shortened").html("<div class='well'><b>Shortened url of "+longurl+" : </b><p id='shorturl'>"+res.url+"</p></div>");
                 else if(res.status_txt == "INVALID_URI")
-                  $("#shortened").html("<div class='well'><b>Status code : "+res.status_code+". This is an "+res.status_txt+"</b></div>");
+                  $("#shortened").html("<div class='well'><b>Status code : "+res.status_code+". Enter the correct URL. Because, This is an "+res.status_txt+"</b></div>");
                 else
                   $("#shortened").html("<div class='well'><b>Status code : "+res.status_code+". Enter URL in the above input field ("+res.status_txt+")</b></div>");
 
-            },
-            error: function(err){
-              alert("error");
-            }
-          }); 
+                  $("#bhistory").show();
+                  $("#history").html("");
       },
     error: function(err){
       $("#shortened").html("<div class='well'><b>"+err+"</b></div>");
@@ -37,6 +26,7 @@ $("#shorten").click(function(){
   $.get("https://api-ssl.bitly.com/v3/user/info?access_token=57f0a8845124ac6c2b207ea57f84daa3b68ef2fc", function(data, status){
     //console.log(data);
     $("#userinfo").html("<div class='well' style='text-align: left'><i> Display name &nbsp;: "+data.data.display_name+" <br>Member since : "+new Date (Number(data.data.member_since))+"</i></div>");
+    $("#buserinfo").hide();
   });
 });
   
@@ -45,8 +35,23 @@ $("#shorten").click(function(){
     //console.log(data);
     for(var i = 0 ; i < data.data.link_history.length ; i++) 
     {
-        console.log(data.data.link_history);
-        $("#history").append("<div class='well' style='text-align: left'>"+
+      $("#bhistory").hide();
+      //console.log(data.data.link_history);
+      if(data.data.link_history[i].title == "")
+      {
+        $("#history").append("<div class='well' style='text-align: left'>Title is not provided"+
+                              "<hr><i><b>Original URL &nbsp;&nbsp; : </b>"+
+                              data.data.link_history[i].long_url+
+                              "<br><b>Shortened URL : </b><a href="
+                              +data.data.link_history[i].link+
+                              " target=_blank>"+
+                              data.data.link_history[i].link+
+                              "</a></i></div>");
+      $("#shortened").html("");
+      }
+     else
+     {
+       $("#history").append("<div class='well' style='text-align: left'>"+
                               data.data.link_history[i].title+
                               "<hr><i><b>Original URL &nbsp;&nbsp; : </b>"+
                               data.data.link_history[i].long_url+
@@ -54,7 +59,9 @@ $("#shorten").click(function(){
                               +data.data.link_history[i].link+
                               " target=_blank>"+
                               data.data.link_history[i].link+
-                              "</a></i><br></div>");
+                              "</a></i></div>");
+     $("#shortened").html("");
+     } 
     }
     });
 });
